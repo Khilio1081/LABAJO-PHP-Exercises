5fada2e066d0e635057aefc8dc4adfd69a597eeb
Lhio Labajo <tertuliolabajoiii@gmail.com>
2024-03-04 11:39:43 +0800

    Adjusted the spacing to be more readable

--- Fibonacci_Series/Fibonacci_series.php
+++ Fibonacci_Series/Fibonacci_series.php
@@ -6,15 +6,18 @@ Class Fibonacci{
     public $n2 = 1;
 
     public function calculate() {
+
         echo "<h3>Fibonacci series for first 10 numbers: </h3>";
         echo $this->n1 . ' ' . $this->n2 . ' ';
+
         while ($this->num < 10 )  
         {  
             $this->n3 = $this->n2 + $this->n1;  
-            echo $this->n3.' ';  
-            $this->n1 = $this->n2;  
-            $this->n2 = $this->n3;  
+            echo $this->n3.' ';
 
+            $this->n1 = $this->n2;
+            $this->n2 = $this->n3;
+            
             $this->num = $this->num + 1;  
         }
     }
@@ -22,4 +25,3 @@ Class Fibonacci{
 
 $obj = new Fibonacci();
 $obj->calculate();
-// 0 1 1 2 3 5 
